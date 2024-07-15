60d08dacef9b4078e1fac0fdd149f142dff77fcf
reschmax <reschmax@msu.edu>
2024-07-15 14:28:35 -0400

    Connected Employee class to main and ensured access to class was given. New classes were created for Artists and Staff that extend the Employee class.

---
+++ Employee.java
@@ -0,0 +1,19 @@
+/*
+ * Employee Class
+ * Author: Max Resch
+ * 
+ * A class that describes the characteristic of a general employee
+ * 
+ */
+public class Employee {
+
+    private String name;
+
+    Employee(String n) {
+
+        name = n;
+    }
+
+    public String getName() {return name;}
+    
+}
--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
+/*
+ * Main class
+ * Author: Max Resch
+ * 
+ * A class that handles the I/O interactivity of the program
+ * 
+ */
 public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World");
+        Employee employee = new Employee("John");
+        System.out.println(employee.getName());
     }
 }
